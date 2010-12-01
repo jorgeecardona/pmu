a7379f02a50eb84e1a74ec1528c7a68593261287
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2010-12-01 14:48:10 -0500

    Robustness in lines

---
+++ pmu/robust_line.py
@@ -0,0 +1,90 @@
+import pmu
+from ga import World
+
+# Filename
+filename = "/home/jcardona/Downloads/ieee14cdf.txt"
+number_of_busses = 14
+
+# Open power system.
+power_system = pmu.PowerSystem.open(filename)
+busses_keys = power_system.busses.keys()
+systems = [power_system]
+
+# Build a set of systems with n-1 branches.
+for i in range(len(power_system.branches)):
+
+    # Add a new system
+    systems.append(
+        pmu.PowerSystem.open(filename))
+
+    # Delete one of the branches in the last system
+    branch = systems[-1].branches.pop(i)
+    branch.disconnect()
+
+
+def fitness(org):
+    # Fit
+    fit = 0
+
+    # This genom has to fit all the systems.
+    for power_system in systems:
+
+        # Reset the pmu locations.
+        power_system.pmu_reset()
+        n = len(busses_keys)
+
+        # Start with a clean locations set
+        locations = []
+        for i in range(n):
+            # If there is a one in the dna, the try to locate a pmu.
+            if org.dna[i] == 1:
+                key = busses_keys[i]
+                # If the branch is up in the system then locate.
+                if key in power_system.busses.keys():
+                    locations.append(key)
+
+        # Pmu number
+        fit += len(locations)
+
+        # Locate pmu.
+        power_system.pmu_location(locations)
+
+        for branch in power_system.branches:
+            if branch.measure == pmu.Measure():
+                fit += 10
+
+        for bus in power_system.busses.values():
+            if bus.measure == pmu.Measure():
+                fit += 20
+
+    return fit
+
+
+w = World(fitness,
+          population=60,
+          dna_size=len(busses_keys),
+          mutation_factor=0.7)
+
+for i in range(100):
+
+    bests = w.new_generation(cut=0.6)
+
+    print "Global: "
+    for i in range(5):
+        for gen in w.bests[i][0].dna:
+            print gen,
+        print w.bests[i][1]
+
+    print "Local: "
+    for p in bests[:10]:
+        for gen in p[0].dna:
+            print gen,
+        print p[1]
+
+    print "New generation"
+
+print "Finally: "
+for p in w.bests:
+    for gen in p[0].dna:
+        print gen,
+    print p[1]
